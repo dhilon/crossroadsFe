32a42e9260520425e067d5940a7a72e5077ae975
dhilon <70111176+dhilon@users.noreply.github.com>
2022-07-11 19:48:30 -0700

    random change

--- src/App.js
+++ src/App.js
@@ -300,7 +300,7 @@ function HTPDialog(props) {
     <Dialog onClose={onClose} open={open}>
       <DialogTitle>How to Play</DialogTitle>
       <Typography variant = "body1" sx = {{padding: 1}}>
-        This game involves the complex natures of game theory and the trustworthiness of cooperation. Choose the side that you think other people will not choose. Whichever side has the least amount of 
+        This game delves into both the complex natures of game theory and the trustworthiness of cooperation. Choose the side that you think other people will not choose. Whichever side has the least amount of 
         votes by the hour will get a certain amount of points added to the score and their streak increased by one. 
         Use other tools on screen to help you decide the road you will take.
         
