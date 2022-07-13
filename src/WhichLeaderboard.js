39eb108ebf20a10c2abef46a5310e967f834bb65
dhilon <70111176+dhilon@users.noreply.github.com>
2022-07-12 19:05:40 -0700

    tried to move leaderboard anchor

--- src/WhichLeaderboard.js
+++ src/WhichLeaderboard.js
@@ -5,6 +5,9 @@ import {Leaderboard, BatteryCharging90Outlined, CardMembershipOutlined} from "@m
 import Divider from '@mui/material/Divider'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import PropTypes from 'prop-types';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 
@@ -42,7 +45,7 @@ function WhichLeaderboard(props) {
           
         </MenuItem>
         <MenuItem onClick={leadClickOpen}>
-          <MostPointsMenu>
+          <MostPointsMenu >
           
           </MostPointsMenu>
         </MenuItem>
@@ -67,24 +70,28 @@ function TopStreaksMenu(props){
       <Button variant = "contained" size = "normal" onClick={handleClick}>
         <Leaderboard fontSize="large" />
           <Typography variant = "h5" sx = {{padding: 1}}>
-            <BatteryCharging90Outlined size = "small"/>
-            Top Streaks
-            <BatteryCharging90Outlined size = "small"/>
+            <CardMembershipOutlined size = "small"/>
+              Top Streaks
+            <CardMembershipOutlined size = "small"/>
           </Typography>
         <Leaderboard fontSize="large"/>
       </Button>
-      <Menu
-        id="leads-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem>1st place: ...</MenuItem>
-        <MenuItem>2nd place: ...</MenuItem>
-        <MenuItem>3rd place: ...</MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem>Your place: ...</MenuItem>
-      </Menu>
+      <Dialog onClose={handleClose} open={open} onClick = {handleClick}>
+        <DialogTitle>Top Streaks Leaderboard</DialogTitle>
+        <Menu
+          id="points-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>1st place: ...</MenuItem>
+          <MenuItem>2nd place: ...</MenuItem>
+          <MenuItem>3rd place: ...</MenuItem>
+          <Divider sx={{ my: 0.5 }} />
+          <MenuItem>Your place: ...</MenuItem>
+        </Menu>
+      </Dialog>
+      
     </div>
   )
 }
@@ -111,23 +118,25 @@ function MostPointsMenu(props){
           </Typography>
         <Leaderboard fontSize="large"/>
       </Button>
-      <Menu
-        id="points-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem>1st place: ...</MenuItem>
-        <MenuItem>2nd place: ...</MenuItem>
-        <MenuItem>3rd place: ...</MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem>Your place: ...</MenuItem>
-      </Menu>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Most Points Leaderboard</DialogTitle>
+        <Menu
+          id="points-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>1st place: ...</MenuItem>
+          <MenuItem>2nd place: ...</MenuItem>
+          <MenuItem>3rd place: ...</MenuItem>
+          <Divider sx={{ my: 0.5 }} />
+          <MenuItem>Your place: ...</MenuItem>
+        </Menu>
+      </Dialog>
+      
     </div>
   )
 }
-
-
 
 
 export default WhichLeaderboard
